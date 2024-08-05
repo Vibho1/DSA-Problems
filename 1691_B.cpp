2812a6c94d97ad633d0f503a7901cace896da471
Vibho <vibhos.vs@gmail.com>
2024-08-05 20:26:30 +0530

    Create 1691_B.cpp

---
+++ 1691_B.cpp
@@ -0,0 +1,50 @@
+// Shoe Shuffling
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int n;
+        cin >> n;
+        
+        vector<int> vec(n + 1);
+        
+        for(int i = 1; i <= n; i++) {
+            cin >> vec[i];
+        }
+        
+        int i = 1, j = 2;
+        bool flag = false;
+        int c = 1;
+        
+        while(i <= n) {
+            while(j <= n && vec[j] == vec[i]) {
+                j++;
+            }
+            if(j - i > 1) {
+                for(int k = j - 1; k >= i; k--) {
+                    vec[c] = k;
+                    c++;
+                }
+            }
+            else {
+                flag = true;
+                break;
+            }
+            i = j;
+        }
+        
+        if(!flag) {
+            for(int l = 1; l <= n; l++) cout << vec[l] << " ";
+        } else {
+            cout << -1;
+        }
+        
+        cout << endl;
+    }
+}
