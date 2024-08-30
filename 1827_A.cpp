5bfc8cb8fbe8c42262ee5e37b0a1476ed6439a74
Vibho <vibhos.vs@gmail.com>
2024-08-30 21:51:43 +0530

    Create 1827_A.cpp

---
+++ 1827_A.cpp
@@ -0,0 +1,54 @@
+// Counting Orders
+
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int n;
+        cin >> n;
+        
+        vector<int> vec1(n);
+        vector<int> vec2(n);
+
+        for(int i = 0; i < n; i++) {
+            cin >> vec1[i];
+        }
+        
+        for(int i = 0; i < n; i++) {
+            cin >> vec2[i];
+        }
+        
+        sort(vec1.begin(), vec1.end());
+        sort(vec2.begin(), vec2.end());
+        
+        bool flag = false;
+        
+        for(int i = 0; i < n; i++) {
+            if(vec1[i] <= vec2[i]) flag = true;
+        }
+        
+        if(!flag) {
+        int j = 0;
+        ll ans = 1;
+        ll mod = 1e9 + 7;
+        
+        for(int i = 0; i < n; i++) {
+            while(j < n && vec2[j] < vec1[i]) {
+                j++;
+            }
+            ans = (ans *  (j - i)) % mod; 
+        }
+        
+        cout << ans;
+        }
+        else cout << 0;
+        
+        cout << "\n";
+    }
+}
